025a9fb4d69752160baa603004765ea4c2a1b59e
Andrey <andrey.o.paula@gmail.com>
2020-02-25 09:18:40 -0300

    Build test

---
+++ build/precache-manifest.d94ec1669924caaae2f15762f267929c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa51b85f95314771574da3ca07487e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ec097ea959e56cfbcb1",
+    "url": "/static/css/main.e2a577fc.chunk.css"
+  },
+  {
+    "revision": "f65325e0ba0a7a0018d2",
+    "url": "/static/js/2.bf85d1c0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.bf85d1c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec097ea959e56cfbcb1",
+    "url": "/static/js/main.49c8c2a0.chunk.js"
+  },
+  {
+    "revision": "d644ff198816bb9f0669",
+    "url": "/static/js/runtime-main.c0627188.js"
+  }
+]);
